544e61092ae322a0436d0ba7a2eebf768a134b70
Garek <Igor.Esipenko@urfu.me>
2023-09-29 22:33:21 +0500

    Нужно больше функций

--- js/functions.js
+++ js/functions.js
@@ -1,10 +1,49 @@
-function checkLenght (string, lenght) {
-  if (string.lenght <= lenght) {
-    console.log(true);
+function checkLenght (inputString, maxLenght) {
+  if (inputString.length <= maxLenght) {
+    return true;
   }
-  console.log(false);
+  else {
+    return false;
+  }
 }
 
 checkLenght('проверяемая строка', 20);
 checkLenght('проверяемая строка', 18);
 checkLenght('проверяемая строка', 10);
+
+function isPalindrom (inputString) {
+  let reverseString = '';
+  let newString = inputString.replaceAll(' ', '');
+  for (let i = newString.length - 1; i >= 0; i--) {
+    reverseString += newString[i];
+  }
+  if (newString.toLowerCase() === reverseString.toLowerCase()) {
+    return true;
+  }
+  else {
+    return false;
+  }
+}
+isPalindrom('топот');
+isPalindrom('ДовОд');
+isPalindrom('Кекс');
+isPalindrom('Лёша на полке клопа нашёл ');
+
+function getCount (inputParam) {
+  let newString = inputParam.toString();
+  let number = '';
+  for (let i = 0; i < newString.length; i++){
+    if (!Number.isNaN(parseInt(newString[i]))) {
+      number += newString[i];
+    }
+  }
+  return parseInt(number);
+}
+getCount('2023 год');
+getCount('ECMAScript 2022');
+getCount('1 кефир, 0.5 батона');
+getCount('агент 007');
+getCount('а я томат');
+getCount(2023);
+getCount(-1);
+getCount(1.5);
