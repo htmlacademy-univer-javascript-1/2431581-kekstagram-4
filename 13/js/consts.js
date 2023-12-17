c9c7a3aceeae20364085d6e2b8d805b45f5bea98
Keks <keks@htmlacademy.ru>
2023-12-17 15:44:11 +0300

    :heavy_check_mark: Сборка #13

---
+++ 13/js/consts.js
@@ -0,0 +1,14 @@
+const Keys = {
+  ESCAPE: 'Escape',
+  ESC: 'Esc'
+};
+
+const COMMENTS_STEP = 5;
+const MAX_STRING_LENGTH = 140;
+const ALERT_SHOW_TIME = 5000;
+const MAX_SYMBOLS = 20;
+const MAX_HASHTAGS = 5;
+const DELAY = 500;
+const FILE_TYPES = ['gif', 'jpg', 'jpeg', 'png'];
+
+export {MAX_SYMBOLS, MAX_HASHTAGS, Keys, COMMENTS_STEP, ALERT_SHOW_TIME, MAX_STRING_LENGTH, DELAY, FILE_TYPES};
